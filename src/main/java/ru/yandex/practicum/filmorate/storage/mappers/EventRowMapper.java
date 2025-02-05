c1d5a04524231b4aa4c054e22f35e2569898775c
YZY <muteallchat30@ya.ru>
2025-02-05 14:30:26 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/mappers/EventRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/EventRowMapper.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.storage.mappers;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.enums.EventType;
+import ru.yandex.practicum.filmorate.model.enums.Operation;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,8 +17,8 @@ public class EventRowMapper implements RowMapper<Event> {
                 .eventId(rs.getInt("event_id"))
                 .timestamp(rs.getTimestamp("timestamp").toInstant().toEpochMilli())
                 .userId(rs.getLong("user_id"))
-                .eventType(Event.EventType.valueOf(rs.getString("event_type")))
-                .operation(Event.Operation.valueOf(rs.getString("operation")))
+                .eventType(EventType.valueOf(rs.getString("event_type")))
+                .operation(Operation.valueOf(rs.getString("operation")))
                 .entityId(rs.getInt("entity_id"))
                 .build();
     }
